6e8feaf27ed6daae9db8c9b5c4d5f00893ffbebe
Katie Millican <gtj0@Katies-MacBook-Pro.local>
2018-02-23 11:44:04 +0000

    Add functions to create midi from separate note duration list and note pitch list

---
+++ midi_creation.py
@@ -0,0 +1,26 @@
+import pretty_midi as pm
+
+def get_pitch_sequence_from_midi(pitch_input):
+    song = pm.PrettyMIDI(pitch_input)
+    inst = song.instruments[0].notes
+    pitch_list = [note.pitch for note in inst]
+    return pitch_list
+
+def create_midi_melody(note_durations, pitch_input, melody_output, duration=0.3):
+    melody = pm.PrettyMIDI()
+    kboard = pm.Instrument(program=0)
+    pitches = get_pitch_sequence_from_midi(pitch_input)
+    end = 0
+    ix = 0
+    for item in note_durations:
+        if item < 0:
+            end = end + (-1 * item * duration)
+            continue
+        start = end
+        end = start + (item * duration)
+        pitch = pitches[ix]
+        ix += 1
+        new_note = pm.Note(velocity=100, pitch=pitch, start=start, end=end)
+        kboard.notes.append(new_note)
+    melody.instruments.append(kboard)
+    melody.write(melody_output)
