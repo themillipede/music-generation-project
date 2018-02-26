aa0eecf1093f6820c21644a5c1a7c35f3008e2bd
Katie Millican <gtj0@Katies-MacBook-Pro.local>
2018-02-26 21:07:16 +0000

    Alter handling of melody such that melody input must be a midi file

--- note_transformation.py
+++ note_transformation.py
@@ -7,14 +7,14 @@ scale_positions = {
 
 scale_steps = {str(k1) + k[0]: v1 for k, v in scale_positions.items() for k1, v1 in v.items()}
 
-def normalise_midi_melody_key(melody, scale_steps, key_code, normalised_melody_output):
+def normalise_midi_melody_key(melody_input, scale_steps, key_code, normalised_melody_output):
     normalised_melody = pm.PrettyMIDI()
     kboard = pm.Instrument(program=0)
     distance_from_standard_root = scale_steps[key_code]
+    melody = pm.PrettyMIDI(melody_input).instruments[0].notes
     for note in melody:
         normalised_pitch = note.pitch - distance_from_standard_root
-        normalised_note = pm.Note(velocity=note.velocity, pitch=normalised_pitch,
-                                  start=note.start, end=note.end)
+        normalised_note = pm.Note(velocity=note.velocity, pitch=normalised_pitch, start=note.start, end=note.end)
         kboard.notes.append(normalised_note)
     normalised_melody.instruments.append(kboard)
     normalised_melody.write(normalised_melody_output)
