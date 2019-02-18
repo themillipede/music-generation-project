34952109bdfb7b1b8a8462aad8913515c2f6cec3
Katherine Millican <katherine.e.millican@gmail.com>
2019-02-18 03:21:30 +0000

    MIDI creation moved to new file

--- midi_creation.py
+++ midi_creation.py
@@ -1,26 +1,34 @@
 import pretty_midi as pm
 
-def get_pitch_sequence_from_midi(pitch_input):
-    song = pm.PrettyMIDI(pitch_input)
-    inst = song.instruments[0].notes
-    pitch_list = [note.pitch for note in inst]
-    return pitch_list
 
-def create_midi_melody(note_durations, pitch_input, melody_output, duration=0.3):
-    melody = pm.PrettyMIDI()
-    kboard = pm.Instrument(program=0)
-    pitches = get_pitch_sequence_from_midi(pitch_input)
-    end = 0
-    ix = 0
-    for item in note_durations:
-        if item < 0:
-            end = round(end + (-1 * item * duration), 1)
-            continue
-        start = end
-        end = round(start + (item * duration), 1)
-        pitch = pitches[ix]
-        ix += 1
-        new_note = pm.Note(velocity=100, pitch=pitch, start=start, end=end)
-        kboard.notes.append(new_note)
-    melody.instruments.append(kboard)
-    melody.write(melody_output)
+class MidiMelody:
+    def __init__(self):
+        self.midi_melody = None
+
+    def _get_pitch_sequence_from_midi(self, pitch_midi):
+        song = pm.PrettyMIDI(pitch_midi)
+        inst = song.instruments[0].notes
+        pitch_list = [note.pitch for note in inst]
+        return pitch_list
+
+    def create_midi_melody(self, relative_durations, pitch_midi, quaver_duration=30):
+        melody = pm.PrettyMIDI()
+        kboard = pm.Instrument(program=0)
+        pitch_list = self._get_pitch_sequence_from_midi(pitch_midi)
+        end = 0
+        idx = 0
+        for item in relative_durations:
+            if item < 0:
+                end = round(end + (-1 * item * quaver_duration))
+                continue
+            start = end
+            end = round(start + (item * quaver_duration))
+            pitch = pitch_list[idx]
+            idx += 1
+            new_note = pm.Note(velocity=100, pitch=pitch, start=start, end=end)
+            kboard.notes.append(new_note)
+        melody.instruments.append(kboard)
+        self.midi_melody = melody
+
+    def write_to_midi_file(self, output_file):
+        self.midi_melody.write(output_file)
