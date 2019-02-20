e57fce582dc8b5623ab8dfadde69fd0ae8e87332
Katherine Millican <katherine.e.millican@gmail.com>
2019-02-20 01:32:08 +0000

    Timestep-creation file added

---
+++ timesteps.py
@@ -0,0 +1,89 @@
+class Piece:
+    def __init__(self, title, composer, pickup_duration, melody, chords, bars):
+        self.title = title
+        self.composer = composer
+        self.pickup_duration = pickup_duration
+        self.timesteps = []
+        this_note = TempObject()
+        this_chord = TempObject()
+        this_bar = TempObject()
+        n = -1
+        c = -1
+        b = -1
+
+        while n < len(melody) or c < len(chords) or b < len(bars):
+            timestep_duration = min(
+                this_note.time_remaining,
+                this_chord.time_remaining,
+                this_bar.time_remaining
+            )
+            if timestep_duration > 0:
+                this_timestep = Timestep(
+                    note_name=this_note.name,
+                    note_octave=this_note.octave,
+                    note_pitch=this_note.pitch,
+                    root=this_chord.root,
+                    bass=this_chord.bass,
+                    chord_type=this_chord.type_chordset,
+                    chord_full=this_chord.full_chordset,
+                    bar_number=this_bar.number,
+                    duration=timestep_duration,
+                    is_tied=False,
+                    is_barline=is_barline,
+                    same_note=same_note
+                )
+                self.timesteps.append(this_timestep)
+            elif self.pickup_duration > 0:
+                this_chord.time_remaining = self.pickup_duration
+                this_bar.time_remaining = self.pickup_duration
+
+            this_note.time_remaining -= timestep_duration
+            if this_note.time_remaining == 0:
+                n += 1
+                if n < len(melody):
+                    this_note = melody[n]
+                same_note = False
+            else:
+                this_timestep.is_tied = True
+                same_note = True
+
+            this_chord.time_remaining -= timestep_duration
+            if this_chord.time_remaining == 0:
+                c += 1
+                if c < len(chords):
+                    this_chord = chords[c]
+
+            this_bar.time_remaining -= timestep_duration
+            if this_bar.time_remaining == 0:
+                b += 1
+                if b < len(bars):
+                    this_bar = bars[b]
+                    is_barline = True
+            else:
+                is_barline = False
+
+
+class Timestep:
+    """
+    Every unique note/chord/bar combination gets its own timestep.
+    """
+    def __init__(self, note_name, note_octave, note_pitch, root, bass, chord_type,
+                 chord_full, bar_number, duration, is_tied, is_barline, same_note):
+        self.note_name = note_name
+        self.note_octave = note_octave
+        self.note_pitch = note_pitch
+        self.root = root
+        self.bass = bass
+        self.chord_type = chord_type
+        self.chord_full = chord_full
+        self.bar_number = bar_number
+        self.duration = duration
+        self.is_tied = is_tied
+        self.is_barline = is_barline
+        self.same_note = same_note
+
+
+class TempObject:
+    def __init__(self):
+        self.duration = 0
+        self.time_remaining = self.duration
