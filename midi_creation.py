dfb2fd413b60315cdd128df3854c41d17f758304
Katherine Millican <katherine.e.millican@gmail.com>
2019-03-03 23:35:29 +0000

    Neatened up midi creation

--- midi_creation.py
+++ midi_creation.py
@@ -1,34 +1,32 @@
 import pretty_midi as pm
 
+from definitions import QUAVER_DURATION
+
 
 class MidiMelody:
-    def __init__(self):
-        self.midi_melody = None
-
-    def _get_pitch_sequence_from_midi(self, pitch_midi):
-        song = pm.PrettyMIDI(pitch_midi)
-        inst = song.instruments[0].notes
-        pitch_list = [note.pitch for note in inst]
-        return pitch_list
-
-    def create_midi_melody(self, relative_durations, pitch_midi, quaver_duration=30):
+    def __init__(self, relative_durations, pitch_midi):
         melody = pm.PrettyMIDI()
         kboard = pm.Instrument(program=0)
-        pitch_list = self._get_pitch_sequence_from_midi(pitch_midi)
+        pitch_list = self.get_melody_pitches(pitch_midi)
         end = 0
         idx = 0
         for item in relative_durations:
-            if item < 0:
-                end = round(end + (-1 * item * quaver_duration))
-                continue
+            duration = abs(item) * QUAVER_DURATION
             start = end
-            end = round(start + (item * quaver_duration))
+            end = round(start + duration)
+            if item < 0:
+                continue
             pitch = pitch_list[idx]
             idx += 1
-            new_note = pm.Note(velocity=100, pitch=pitch, start=start, end=end)
+            new_note = pm.Note(100, pitch, start, end)
             kboard.notes.append(new_note)
         melody.instruments.append(kboard)
         self.midi_melody = melody
 
-    def write_to_midi_file(self, output_file):
+    def get_melody_pitches(self, pitch_midi):
+        song = pm.PrettyMIDI(pitch_midi)
+        inst = song.instruments[0].notes
+        return [note.pitch for note in inst]
+
+    def write_midi_to_file(self, output_file):
         self.midi_melody.write(output_file)
