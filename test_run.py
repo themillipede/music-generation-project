7d6e3e490a0af748e69f1dede108fd1463849d6e
Katie Millican <katie@millican.org>
2019-03-11 21:51:19 +0000

    Added timestep vector creation function

--- test_run.py
+++ test_run.py
@@ -7,10 +7,21 @@ from chord_parsing import ChordProgression
 from bar_parsing import BarSequence
 from timesteps import Piece
 
+
+def timestep_vectors(piece):
+    note_pitch = np.eye(38, dtype=int)[
+        [36 if ts.same_note else ts.note_pitch - 48 if ts.note_pitch > 0 else 37 for ts in piece]
+    ]
+    root = np.eye(13, dtype=int)[[note_name_idx[ts.root] if ts.root else 12 for ts in piece]]
+    bass = np.eye(13, dtype=int)[[note_name_idx[ts.bass] if ts.bass else 12 for ts in piece]]
+    chord = np.array([[i in ts.full_chordset for i in range(12)] for ts in piece], dtype=int)
+    duration = np.eye(24, dtype=int)[[int(ts.duration / 10 - 1) for ts in piece]]
+    return np.concatenate([note_pitch, root, bass, chord, duration], axis=1)
+
+
 with open('test_data.json') as f:
     piece_data = json.load(f)
 
-piece_list = []
 for title, details in piece_data.items():
     print(title)
     if not details["chords"]:
@@ -19,18 +30,5 @@ for title, details in piece_data.items():
     chords = ChordProgression(details['chords'])
     bars = BarSequence(eval(details['bars']))
     piece = Piece(title, details['composer'], details['pickup'], melody.melody, chords.chords, bars.bars)
-    piece_list.append(piece)
-
-
-def timestep_vectors(piece):
-    note_pitch = np.eye(38)[[36 if ts.same_note else ts.note_pitch - 48 if ts.note_pitch > 0 else 37 for ts in piece]]
-    root = np.eye(13)[[note_name_idx[ts.root] if ts.root else 12 for ts in piece]]
-    bass = np.eye(13)[[note_name_idx[ts.bass] if ts.bass else 12 for ts in piece]]
-    chord = np.array([
-        np.array([int(i) for i in format(sum([2 ** (11 - n) for n in ts.full_chordset]), 'b').zfill(12)])
-        for ts in piece
-    ])
-    duration = np.eye(24)[[int(ts.duration / 10 - 1) for ts in piece]]
-
-
-print(timestep_vectors(piece_list[1].timesteps))
+    vectors = timestep_vectors(piece.timesteps)
+    print(vectors)
